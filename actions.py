2f50be057d6eda161e0efb9a39aa80f16bbc732e
Avocado Rider <st3lla_programming@protonmail.com>
2024-10-24 21:19:08 +1300

    creates runable commands

--- actions.py
+++ actions.py
@@ -1,8 +1,7 @@
-import os
 import random
-from math import ceil
 from config import Config
-from options import Strategy, Schedule, MutatorType, DisableTrimming, OldQueueCycling, MOptMutator, Option, EnvironmentVariable
+from options import EnvironmentVariable, Option
+from typing import Optional, Sequence
 
 #We have a config that we need to then turn into what we need the actions to do
 
@@ -10,91 +9,61 @@ from options import Strategy, Schedule, MutatorType, DisableTrimming, OldQueueCy
 class Actions:
     def __init__(self, config: Config):
         self.config = config
-
-        if self.config.instances == 0:
-            self.config.instances = len(os.sched_getaffinity(0))
-        self.instances:list[Instance] = []
-
-        self.power_schedule = self.setup_power_schedule()
-        self.secondary_options = self.setup_secondary_options()
-
-        self.setup_instances()
-
-    def setup_power_schedule(self):
-        power_schedule = []
-        strategies = ['explore', 'coe', 'lin', 'quad', 'exploit', 'rare']
-
-
-        for strategy in strategies:
-            count = int(self.config.config_data['power_schedule'][strategy])
-            power_schedule.extend([Schedule(strategy) for _ in range(count)])
-        
-        assert len(power_schedule) < self.config.instances, "Power schedule values exceed the number of instances"
-        return power_schedule
-        
-    def setup_secondary_options(self):
-        secondary_options = []
-        option_table : dict[str, Option] = {
-            'MOpt_mutator': MOptMutator(),
-            'old_queue_cycle': OldQueueCycling(),
-            'disable_trimming': DisableTrimming(),
-            'explore_strategy': Strategy('explore'),
-            'exploit_strategy': Strategy('exploit'),
-            'ascii_type': MutatorType('ascii'),
-            'binary_type': MutatorType('binary')
-        }
-
-        for option, obj in option_table.items():
-            count = ceil(self.config.config_data['secondary_options'][option] * self.config.instances)
-            count = max(0, count)
-            secondary_options.append([obj for _ in range(count)])
-        return secondary_options
+        self.instances = self.setup_instances()
 
     def setup_instances(self):
-        random_power_schedule = self.power_schedule.copy()
-        random_power_schedule.extend([None] * (self.config.instances - len(random_power_schedule)))
-        random.shuffle(random_power_schedule)
+        instances: list['Instance'] = []
 
+        # Full options containing None values so we get an even distribution
+        full_secondary_options: list[list[Optional[Option]]] = []
+        for option in self.config.secondary_options:
+            option_copy = option.copy()
+            option_copy.extend([None] * (self.config.instances - len(option)))
 
-        random_secondary_options = []
-        # Create a copy of secondary_options to avoid modifying the originala
-        for options in self.secondary_options:
-            options_copy = options.copy()
-            options_copy.extend([None] * (self.config.instances - len(options_copy)))
-            random.shuffle(options_copy)
-            random_secondary_options.append(options_copy)
+            random.shuffle(option_copy)
+            full_secondary_options.append(option_copy)
 
-        for _ in range(self.config.instances):
-            power_schedule = random_power_schedule.pop()
-            secondary_option = [option.pop() for option in random_secondary_options]
+        # Create instances by popping the last value from each option and only adding if it's not None
+        for i in range(self.config.instances):
+            secondary_options = []
+            for option in full_secondary_options:
+                option_value = option.pop()
+                if option_value:
+                    secondary_options.append(option_value)
+
+            # If we don't want a master instance, we skip the first one
+            if self.config.no_master:
+                i += 1
         
-
             new_instance = Instance(self.config.input, 
                                     self.config.output, 
-                                    memory=self.config.memory, 
-                                    timeout=self.config.timeout, 
-                                    dict=self.config.dict, 
-                                    power_schedule=power_schedule,
-                                    secondary_options=secondary_option)
-            self.instances.append(new_instance)
+                                    secondary_options,
+                                    i,
+                                    self.config.memory,
+                                    self.config.timeout,
+                                    self.config.dict)
+            
+            instances.append(new_instance)
+
+        return instances
 
 class Instance:
-    start_command = "afl-fuzz -i {} -m {} -o {} -t {}"
-
-    def __init__(self, input, output, memory='0', timeout='+', dict=None, power_schedule=None, secondary_options=None):
+    start_command = "afl-fuzz {} -i {} -o {}"
+    def __init__(self, input, output, secondary_options: list[Option], count, name=None, memory='0', timeout="'+'", dict=None):
         self.input = input
         self.output = output
+        self.secondary_options = secondary_options
+        self.count = count
+        if self.count == 0:
+            self.main = True
+        else:
+            self.main = False
+        self.name = name
         self.memory = memory
         self.timeout = timeout
         self.dict = dict
-        self.power_schedule = power_schedule
-        self.secondary_options = [option for option in secondary_options if option]
     
     def get_command(self) -> str:
-
-        def flatten(_list):
-            return [i for xs in _list for i in xs]
-
         command = []
         env = []
         for option in self.secondary_options:
@@ -104,15 +73,24 @@ class Instance:
                 else:
                     command.append(option.command_string)
         
-        env_joined = ' '.join(env)
-        if env_joined:
-            env_joined += ' '
+        env_joined = ' '.join(env) + ' ' if env else ''
+        command_joined = ' '.join(command) + ' ' if command else ''
+        name = self.name + '-' if self.name else ''
+       
 
-        command_joined = ' '.join(command)
+        if self.main:
+            afl_fuzzer_name = '-M main' + name
+        else:
+            afl_fuzzer_name = '-S secondary' + name + '-' + str(self.count)
 
-        afl_command = Instance.start_command.format(self.input, self.memory, self.output, self.timeout)
-        if afl_command:
-            afl_command += ' '
 
+        afl_command = f"{env_joined}afl-fuzz {afl_fuzzer_name} -i {self.input} -o {self.output}"
+        if self.memory:
+            afl_command += f" -m {self.memory}"
+        if self.timeout:
+            afl_command += f" -t {self.timeout}"
+        if self.dict:
+            afl_command += f" -x {self.dict}"
 
-        return env_joined + afl_command + command_joined
+        afl_command += ' ' + command_joined
+        return afl_command
--- config.py
+++ config.py
@@ -1,19 +1,73 @@
 from argparse import Namespace
 import tomllib
+from options import Strategy, Schedule, MutatorType, DisableTrimming, OldQueueCycling, MOptMutator, Option
+from math import ceil
+import os
 
 class Config:
     def __init__(self, args: Namespace):
         self.input = args.i
         self.memory = args.m
+
         self.instances = args.n
+        if self.instances == 0:
+            self.instances = len(os.sched_getaffinity(0))
+
         self.name = args.name
         self.no_master = args.no_master
         self.output = args.o
-        self.timeout = args.t
+        self.timeout: int | str = args.t
+        if self.timeout == 0:
+            self.timeout = '+'
+
         self.dict = args.x
-        self.config = args.c
+        self.config_location = args.c
         self.config_data = self.load_config()
+        self.secondary_options = self.setup_secondary_options() + [self.setup_power_schedule()]
+
+    def setup_power_schedule(self):
+        power_schedule = []
+        strategies = ['explore', 'coe', 'lin', 'quad', 'exploit', 'rare']
+
+        for strategy in strategies:
+            count = int(self.config_data['power_schedule'][strategy])
+            power_schedule.extend([Schedule(strategy) for _ in range(count)])
+        
+        assert len(power_schedule) < self.instances, 'Power schedule values exceed the number of instances'
+        return power_schedule
+
+
+    def setup_secondary_options(self):
+
+        secondary_options: list[list[Option]] = []
+        option_table : dict[str, Option] = {
+            'MOpt_mutator': MOptMutator(),
+            'old_queue_cycle': OldQueueCycling(),
+            'disable_trimming': DisableTrimming(),
+            'explore_strategy': Strategy('explore'),
+            'exploit_strategy': Strategy('exploit'),
+            'ascii_type': MutatorType('ascii'),
+            'binary_type': MutatorType('binary')
+        }
+
+        strategies = []
+        mutator_types = []
+
+        for option, obj in option_table.items():
+            count = ceil(self.config_data['secondary_options'][option] * self.instances)
+            count = max(0, count)
+
+            # Group Strategy and MutatorType into their respective lists
+            if isinstance(obj, Strategy):
+                strategies.extend([obj for _ in range(count)])
+            elif isinstance(obj, MutatorType):
+                mutator_types.extend([obj for _ in range(count)])
+            else:
+                secondary_options.append([obj for _ in range(count)])
+
+        return secondary_options + [strategies, mutator_types]
+
 
     def load_config(self):
-        with open(self.config) as f:
+        with open(self.config_location) as f:
             return tomllib.loads(f.read())
